0aad400070b465b52bf4458ef9a75cc3f642c4a5
Steve Lukis <20234730+stevelukis@users.noreply.github.com>
2022-10-20 15:08:10 +0800

    Add OrderAdmin

--- store/admin.py
+++ store/admin.py
@@ -2,11 +2,22 @@ from django.contrib import admin
 from . import models
 
 
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'placed_at', 'customer']
+    list_per_page = 10
+    list_select_related = ['customer']
+
+
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price', 'inventory_status']
+    list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
     list_editable = ['unit_price']
     list_per_page = 10
+    list_select_related = ['collection']
+
+    def collection_title(self, product):
+        return product.collection.title
 
     @admin.display(ordering='inventory')
     def inventory_status(self, product):
