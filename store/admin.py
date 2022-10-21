13fd6985a77afcc0402447433a7f8acd37eed87b
Steve Lukis <20234730+stevelukis@users.noreply.github.com>
2022-10-21 10:07:03 +0800

    Add action clear inventory

--- store/admin.py
+++ store/admin.py
@@ -1,4 +1,4 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 from django.db.models import Count, QuerySet
 from django.utils.html import format_html, urlencode
 from django.urls import reverse
@@ -29,6 +29,7 @@ class InventoryFilter(admin.SimpleListFilter):
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
+    actions = ['clear_inventory']
     list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
     list_editable = ['unit_price']
     list_filter = ['collection', 'last_update', InventoryFilter]
@@ -43,6 +44,15 @@ class ProductAdmin(admin.ModelAdmin):
         if product.inventory < 10:
             return 'Low'
         return 'OK'
+
+    @admin.action(description='Clear inventory')
+    def clear_inventory(self, request, queryset: QuerySet):
+        products_count = queryset.update(inventory=0)
+        self.message_user(
+            request,
+            f"${products_count} products successfully updated",
+            messages.SUCCESS
+        )
 
 
 @admin.register(models.Customer)
