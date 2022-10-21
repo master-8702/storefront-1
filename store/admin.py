9c978e41c610d1ef22cd660787f2d41b8033b40b
Steve Lukis <20234730+stevelukis@users.noreply.github.com>
2022-10-21 11:03:46 +0800

    Change tags implementation to Product in admin so that store and tags apps aren't dependent on each other

--- store/admin.py
+++ store/admin.py
@@ -1,10 +1,8 @@
 from django.contrib import admin, messages
-from django.contrib.contenttypes.admin import GenericTabularInline
 from django.db.models import Count, QuerySet
-from django.utils.html import format_html, urlencode
 from django.urls import reverse
+from django.utils.html import format_html, urlencode
 
-from tags.models import TaggedItem
 from . import models
 
 
@@ -37,16 +35,10 @@ class InventoryFilter(admin.SimpleListFilter):
             return queryset.filter(inventory__lt=10)
 
 
-class TagInline(GenericTabularInline):
-    model = TaggedItem
-    autocomplete_fields = ["tag"]
-
-
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
     actions = ['clear_inventory']
     autocomplete_fields = ['collection']
-    inlines = [TagInline]
     search_fields = ['title__icontains']
     prepopulated_fields = {'slug': ['title']}
     list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
