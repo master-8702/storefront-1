3c00d1201d50b09bc6a7ef519b596624487e114b
Steve Lukis <20234730+stevelukis@users.noreply.github.com>
2022-10-27 20:21:38 +0800

    Add searching for products api

--- store/views.py
+++ store/views.py
@@ -3,6 +3,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.filters import SearchFilter
 
 from .filters import ProductFilter
 from .models import Product, Collection, OrderItem, Review
@@ -12,8 +13,9 @@ from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializ
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, SearchFilter]
     filterset_class = ProductFilter
+    search_fields = ['title', 'description', 'collection__title']
 
     def get_serializer_context(self):
         return {'request': self.request}
