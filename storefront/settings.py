254f5589dc6ff36ced8d81be3d32f7d2d446f132
Steve Lukis <20234730+stevelukis@users.noreply.github.com>
2022-11-15 16:20:35 +0800

    Update database settings

--- storefront/settings.py
+++ storefront/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'storefront2',
+        'NAME': 'storefront3',
         'HOST': 'localhost',
         'USER': 'root',
         'PASSWORD': 'password'
