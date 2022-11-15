098e8f6615a6a70e36a61ead7ececf2ca5760d46
Steve Lukis <20234730+stevelukis@users.noreply.github.com>
2022-11-15 19:50:52 +0800

    Add email configuration

--- playground/views.py
+++ playground/views.py
@@ -1,7 +1,15 @@
+from django.core.mail import EmailMessage, BadHeaderError
+from templated_mail.mail import BaseEmailMessage
 from django.shortcuts import render
-from django.http import HttpResponse
-from store.models import Product
 
 
 def say_hello(request):
+    try:
+        message = BaseEmailMessage(
+            template_name='emails/hello.html',
+            context={'name': 'Steve'}
+        )
+        message.send(to=['john@google.com'])
+    except BadHeaderError:
+        pass
     return render(request, 'hello.html', {'name': 'Mosh'})
