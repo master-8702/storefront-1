1dcc6bd010615d42bbdbcfd80599fa7a74650bcc
Steve Lukis <20234730+stevelukis@users.noreply.github.com>
2022-11-18 12:11:40 +0800

    Add TestCreateProduct

---
+++ store/tests/test_products.py
@@ -0,0 +1,66 @@
+import pytest
+from model_bakery import baker
+from rest_framework import status
+
+from store.models import Collection
+
+
+@pytest.fixture
+def create_product(api_client):
+    def do_create_product(product):
+        return api_client.post('/store/products/', product)
+
+    return do_create_product
+
+
+@pytest.mark.django_db
+class TestCreateProduct:
+    def test_if_user_is_admin_returns_201(self, authenticate, create_product):
+        authenticate(is_staff=True)
+        collection = baker.make(Collection)
+
+        response = create_product({'title': 'a',
+                                   'description': 'vb',
+                                   'slug': 'a-b',
+                                   'unit_price': 1,
+                                   'inventory': 1,
+                                   'collection': collection.id})
+
+        assert response.status_code == status.HTTP_201_CREATED
+
+    def test_if_anonymous_returns_401(self, authenticate, create_product):
+        collection = baker.make(Collection)
+
+        response = create_product({'title': 'a',
+                                   'description': 'vb',
+                                   'slug': 'a-b',
+                                   'unit_price': 1,
+                                   'inventory': 1,
+                                   'collection': collection.id})
+
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_if_user_is_not_admin_returns_403(self, authenticate, create_product):
+        authenticate(is_staff=False)
+        collection = baker.make(Collection)
+
+        response = create_product({'title': 'a',
+                                   'description': 'vb',
+                                   'slug': 'a-b',
+                                   'unit_price': 1,
+                                   'inventory': 1,
+                                   'collection': collection.id})
+
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_if_data_is_invalid_returns_400(self, authenticate, create_product):
+        authenticate(is_staff=True)
+
+        response = create_product({'title': '',
+                                   'description': '',
+                                   'slug': '',
+                                   'unit_price': '',
+                                   'inventory': '',
+                                   'collection': ''})
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
