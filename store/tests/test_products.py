bfdbc79130a2639ad60ab20b9d7985b0a7a0828e
master-8702 <55705389+master-8702@users.noreply.github.com>
2023-02-27 15:37:05 +0300

    add update and delete test cases to test_products

--- store/tests/test_products.py
+++ store/tests/test_products.py
@@ -71,11 +71,56 @@ class TestRetrieveProduct:
     def test_if_product_exists_return_200(self, api_client):
         product = baker.make(Product)
 
-        response = api_client.get(f'/store/collections/{product.id}/')
+        response = api_client.get(f'/store/products/{product.id}/')
 
         assert response.status_code == status.HTTP_200_OK
 
     def test_if_product_doesnt_exists_return_404(self, api_client):
-        response = api_client.get('/store/collections/9999/')
+        response = api_client.get('/store/products/9999/')
 
         assert response.status_code == status.HTTP_404_NOT_FOUND
+
+
+
+@pytest.mark.django_db
+class TestUpdateProduct:
+    def test_if_user_is_admin_data_is_valid_returns_200(self,api_client , authenticate):
+        authenticate(is_staff=True)
+        product = baker.make(Product)
+
+        response = api_client.patch(f'/store/products/{product.id}/', {'title': 'changed_product_title'})
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data['title'] == "changed_product_title"
+
+
+    def test_if_user_is_authenticated_data_is_valid_returns_403(self,api_client , authenticate):
+        authenticate()
+        product = baker.make(Product)
+
+        response = api_client.patch(f'/store/products/{product.id}/', {'title': 'changed_product_title'})
+
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+
+
+@pytest.mark.django_db
+class TestDeleteProduct:
+
+    def test_if_user_is_admin_returns_204(self,api_client , authenticate):
+        authenticate(is_staff=True)
+        product = baker.make(Product)
+
+        response = api_client.delete(f'/store/products/{product.id}/')
+
+        assert response.status_code == status.HTTP_204_NO_CONTENT
+
+
+    def test_if_user_is_not_admin_returns_403(self,api_client , authenticate):
+        authenticate()
+        product = baker.make(Product)
+
+        response = api_client.delete(f'/store/products/{product.id}/')
+
+        assert response.status_code == status.HTTP_403_FORBIDDEN
